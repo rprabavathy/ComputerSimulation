11bc45159ec898fac88b4ca88e46e21dd1123fc4
Prabavathy <119755930+rprabavathy@users.noreply.github.com>
2024-04-13 20:31:26 +0200

    Update magic.m

--- Matlab/Solution_4/magic.m
+++ Matlab/Solution_4/magic.m
@@ -1,7 +1,5 @@
 % check whether the generator satisfies the 3 Magic Conditions of Linear Congruence
 % Generator
-%Prabavathy Rajasekaran(2130757
-% Anjaly Kuriakose(2132537)
 
 function magic(a,c,m)
     if((intersect(factor(m),factor(c))~=0) || (mod(a-1,m)~=0) || ((mod(a-1,4)~=0 && mod(m,4)~=0)))
